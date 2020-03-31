3b365800c0a6cfbd5d2c9086ea34c4ce5c9c767f
AliTechGeek52 <mohammadaliraza472@gmail.com>
2020-04-01 01:46:52 +0500

    Updates

---
+++ precache-manifest.6f5dd895f899c4161f68c9e3edceb64b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d583fc363f33a62d3a002073d80fe2",
+    "url": "/pak-rashan-react-version-/index.html"
+  },
+  {
+    "revision": "9b07d6a50731578f12c4",
+    "url": "/pak-rashan-react-version-/static/css/main.865f445e.chunk.css"
+  },
+  {
+    "revision": "71229c1c2a04f77a7f38",
+    "url": "/pak-rashan-react-version-/static/js/2.4b0bffa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pak-rashan-react-version-/static/js/2.4b0bffa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b07d6a50731578f12c4",
+    "url": "/pak-rashan-react-version-/static/js/main.35931710.chunk.js"
+  },
+  {
+    "revision": "ab7b9be816284e5a5d3c",
+    "url": "/pak-rashan-react-version-/static/js/runtime-main.e3f3c185.js"
+  },
+  {
+    "revision": "e9f2008a3c439d069525670e333edc24",
+    "url": "/pak-rashan-react-version-/static/media/banner.e9f2008a.jpg"
+  }
+]);
